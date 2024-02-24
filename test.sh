a0d1983f43484a5e8ba3dc646f9798d6a4d91508
Matthew Cooper <coopertronics@gmail.com>
2024-02-24 18:56:18 +0000

    Should now be passing the array.

--- test.sh
+++ test.sh
@@ -7,4 +7,25 @@
 #currentDir=${result:-/}
 #echo $currentDir
 
+function fillArray() {
+    declare -n arrayName=$1
 
+    for i in 0 1 2 3; do
+        arrayName+=("new item $i")
+    done
+
+    echo "Tot items in function: ${#arrayName[@]}"
+    for item in "${arrayName[@]}"; do
+        echo $item
+    done
+    echo
+}
+
+myArray=("my item 0" "my item 1")
+
+fillArray myArray
+
+echo "Tot items in main: ${#myArray[@]}"
+for item in "${myArray[@]}"; do
+    echo $item
+done
