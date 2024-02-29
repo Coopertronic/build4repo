6aa05048e6975e7f65bee9a849137d5ede3f07f0
Matthew Cooper <coopertronics@gmail.com>
2024-02-29 22:05:54 +0000

    The test now works and passes back the array because I have declaresd a global array and used it. Not the best wa ,but it's what works at this point.

--- test.sh
+++ test.sh
@@ -12,11 +12,9 @@ declare -a thFiles
 clear
 function get_list() {
     ##  Fill array
-    lineBreak="<~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>"
     local thLocation=$1
     local thPrefix=$2
     local thExtention=$3
-    #thFiles=()
     echo "The Path: " $thLocation
     echo "The Prefix: " $thPrefix
     echo "The Extention: " $thExtention
@@ -30,32 +28,25 @@ function get_list() {
             thFiles+=($fileName)
         fi
     done
-    ##  make delimited string
     echo $lineBreak
     echo "Here are all the $thExtention files apart from the newest one."
     echo "Total files in function: ${#thFiles[@]}"
     count=0
-    #thFilesString=""
     for i in "${thFiles[@]}"; do
         echo "File $((count + 1)): "$i
-        #thFilesString="${thFilesString}${i},"
         ((count++))
     done
     echo $lineBreak
-    #echo "The string to be passed:"
-    #echo "$thFilesString"
 }
 ##  Testing stuff
-#if !( get_list $thRepo $currentDir "zst" ); then
-#    something_wrong
-#else
-get_list $thRepo $currentDir "zst"
-echo "Source functions worked."
-echo "See above for results."
-echo "See what has been passed?"
-to_continue
-echo "Here is the string that was passed:"
-echo "${thFiles[@]}"
-#fi
-
-#get_list $thRepo $currentDir "zst" returnString ; zstFiles=${returnString}
+if !( get_list $thRepo $currentDir "zst" ); then
+    something_wrong
+else
+    get_list $thRepo $currentDir "zst"
+    echo "Source functions worked."
+    echo "See above for results."
+    echo "See what has been passed?"
+    to_continue
+    echo "Here is the array that was passed:"
+    echo "${thFiles[@]}"
+fi
